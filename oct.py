2c028fa88362f55c71dd513ac2b32e957f2ab540
Neva <nb20pygeek@gmail.com>
2021-01-18 11:02:55 -0600

    Commit Original files

---
+++ oct.py
@@ -0,0 +1,20 @@
+import turtle
+
+neva=turtle.Turtle()
+neva.color("red")
+neva.penup()
+neva.goto(-75, +75)
+neva.pendown()
+
+
+for i in range(0,8):
+    neva.forward(100)
+    neva.right(45)
+
+
+
+
+
+
+
+
---
+++ readme.md
@@ -0,0 +1,6 @@
+#Draw an Octagon
+
+##Sample program for Github load
+
+NB on January 18, 2021
+============
